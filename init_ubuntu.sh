144c7bbcf78b255fe42464acc5e01a84f6e52f06
kiuchi.keisuke <kiuchi@ubuntu.(none)>
2013-10-25 09:18:49 -0700

    fix sudo

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,29 +1,24 @@
 #!/bin/sh
 
-if [ $(whoami) != "root" ]; then
-echo "You must be root to do!" 2>&1
-    exit 1
-fi
-
 #
 #1. update/upgrade
 #
-apt-get update
-apt-get upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 echo "update/upgrade finished"
 
 #
 #2. install tools
 #
-apt-get install -y curl git-core vim wget open-ssh xsel
+sudo apt-get install -y curl git-core vim wget open-ssh xsel
 echo "install tools finished"
 
 #
 #3. install java
 #
-add-apt-repository -y ppa:webupd8team/java
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y oracle-java7-installer
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y oracle-java7-installer
 echo "install java finished"
 
 #
@@ -42,9 +37,9 @@ echo "install scala finished"
 #
 #5. install Emacs
 #
-add-apt-repository -y ppa:cassou/emacs
-apt-get update
-apt-get install -y emacs24 emacs24-el
+sudo add-apt-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el
 echo "install Emacs finished"
 
 #
